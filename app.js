e9a8947957eaff799b2f7af1683809a72b0c96ac
rizan21 <rizanfarooqui@gmail.com>
2023-01-02 21:07:00 +0530

    click changes color, dragging doesnt work

--- app.js
+++ app.js
@@ -13,22 +13,46 @@ print.addEventListener('click', function() {
 
 // get size from slider and add n*n divs to the grid
 function addDivs(size) {
-    let grid = document.getElementById('grid');
-    grid.innerHTML = '';
+    let grid1 = document.getElementById('grid');
+    grid1.innerHTML = '';
     for (let i = 0; i < size; i++) {
+        let col= document.createElement('div');
+        col.className = 'grid-col';
         for (let j = 0; j < size; j++) {
             let div = document.createElement('div');
             div.className = 'grid-item';
-            grid.appendChild(div);
+            col.appendChild(div);
         }
+        grid1.appendChild(col);
     }
 }
 
 //get size 
-let size = document.getElementById('myRange');
-console.log(size.value);
-size.addEventListener('change', function() {
-    addDivs(size.value);
+let size = 16;
+addDivs(16);
+console.log(size);
+addDivs(size);
+// size.addEventListener('change', function() {
+//     addDivs(size);
+// }
+// );
+
+// on click of clear button clear the grid
+let clear = document.getElementById('clear');
+clear.addEventListener('click', function() {
+    let grid1 = document.getElementById('grid');
+    grid1.innerHTML = '';
+    addDivs(size);
+    // grid = document.getElementsByClassName('grid-item');
 }
 );
+// on click of grid-item change the color of the grid-item to black
+let grid = document.getElementsByClassName('grid-item');
+for (let i = 0; i < grid.length; i++) {
+    grid[i].addEventListener('mousedown', function() {
+        grid[i].style.backgroundColor = 'black';
+    });
+}
+
+
 
